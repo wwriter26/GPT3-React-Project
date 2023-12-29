b64231677cd346dcc67f6cf68a3bcec62ff21ddf
William Writer <111819260+wwriter26@users.noreply.github.com>
2023-12-28 18:25:43 -0700

    Added all of the finished files
    Created a well structured and formatted React Project

---
+++ src/components/brand/imports.js
@@ -0,0 +1,14 @@
+//helper file to import all the images which then we can import them in one line
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export {
+  google,
+  slack,  
+  atlassian,
+  dropbox,
+  shopify,
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root')); // The one div element that is inside of the basic html file.
